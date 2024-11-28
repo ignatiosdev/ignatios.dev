94f259970320e501822bbacc405772202c3b088b
cattalurdai <cattalurdai@gmail.com>
2024-11-28 15:03:53 -0300

    feat(experience): populate information

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -4,26 +4,35 @@ const experienceData: Experience[] = [
   {
     title: { en: "IT Technician at Infoserv", es: "Técnico de IT en Infoserv" },
     description: {
-      es: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, qui nostrud exercitation ullamco laboris nisi ut aliquip Ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
-      en: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, qui nostrud exercitation ullamco laboris nisi ut aliquip Ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
+      es: "Gestión y soporte en más de 100 redes corporativas a nivel nacional, administración de redes, VoIP, servidores, bases de datos y sistemas operativos. Configuración de routers, VPNs, controles de acceso, switches, máquinas virtuales y puntos de acceso para garantizar operaciones de red seguras y eficientes, con un enfoque en brindar a los usuarios un servicio confiable y de alta calidad.",
+      en: "Management and support of over 100 corporate networks nationwide, administration of networks, VoIP, servers, databases, and operating systems. Configuration of routers, VPNs, access controls, switches, virtual machines, and access points to ensure secure and efficient network operations, with a focus on delivering reliable and high-quality service to users.",
     },
     date: { en: "OCTOBER, 2023 - PRESENT", es: "OCTUBRE, 2023 - ACTUALMENTE" },
   },
   {
-    title: { en: "IT Technician at Infoserv", es: "Técnico de IT en Infoserv" },
-    description: {
-      es: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip Ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
-      en: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip Ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
+    title: {
+      en: "Full-Stack Developer at Abbate Servicios Funebres",
+      es: "Desarrollador Full-Stack en Abbate Servicios Fúnebres",
     },
-    date: { en: "OCTOBER, 2023 - PRESENT", es: "OCTUBRE, 2023 - ACTUALMENTE" },
+    description: {
+      es: "Desarrollo de una aplicación web para consultas de horarios de servicio en www.abbate.uy, utilizando React.js y TailwindCSS para una interfaz responsiva. Implementación de un servidor Node.js con una base de datos SQL para la gestión de datos y despliegue de la aplicación en un entorno contenedor con Docker para escalabilidad y fiabilidad.",
+      en: "Development of a web application for service schedule queries at www.abbate.uy, using React.js and TailwindCSS for a responsive interface. Implementation of a Node.js server with an SQL database for data management and deployment of the application in a containerized environment using Docker for scalability and reliability.",
+    },
+    date: { en: "APRIL, 2024 - AUGUST 2024", es: "ABRIL, 2024 - AGOSTO, 2024" },
   },
   {
-    title: { en: "IT Technician at Infoserv", es: "Técnico de IT en Infoserv" },
-    description: {
-      es: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip Ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
-      en: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip Ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum",
+    title: {
+      en: "Front-End Developer Intern at Upcamp",
+      es: "Pasante como Desarrollador Front-End en Upcamp",
     },
-    date: { en: "OCTOBER, 2023 - PRESENT", es: "OCTUBRE, 2023 - ACTUALMENTE" },
+    description: {
+      es: 'Colaboración en equipo para el desarrollo de una simulación frontend de interfaz bancaria llamada "DigitalBank" utilizando React.js y Node.js. Implementación de Tailwind CSS para un diseño limpio y responsive, priorizando la experiencia del usuario. Aplicación de la metodología SCRUM para asegurar un trabajo en equipo efectivo.',
+      en: 'Collaboration with a team to develop a simulated "DigitalBank" banking interface using React.js and Node.js. Implementation of Tailwind CSS for a clean and responsive design, prioritizing user experience. Application of the SCRUM methodology to ensure effective teamwork and timely progress.',
+    },
+    date: {
+      en: "MARCH, 2022 - MAY 2024",
+      es: "MARZO, 2022 - MAYO, 2024",
+    },
   },
 ];
 
