7b3aed407340ffc17f79a1ffde25e3639296737f
Ignacio Cattalurda <icattalurda@teletipos.com.uy>
2025-03-13 01:01:11 -0300

    add new experience

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -2,12 +2,26 @@ import Experience from "@/types/Experience";
 
 const experienceData: Experience[] = [
   {
+    title: { en: "Full-Stack Developer at Teletipos", es: "Desarrollador Full-Stack en Teletipos" },
+    description: {
+      es: "Desarrollo de aplicaciones empresariales para usuarios finales y administrativas, enfocadas en la gestión de datos y la biometría. Implementación de React en el frontend, Node.js y Express para el desarrollo de APIs RESTful en el backend, y MySQL para la gestión de bases de datos. Realización de documentación técnica y trabajo en equipo bajo metodologías ágiles.",
+      en: "Development of enterprise applications for end users, focusing on data management and biometrics. Implementation of React for the frontend, Node.js and Express for RESTful API development on the backend, and MySQL for database management. Creation of technical documentation and collaboration within an agile team environment.",
+    },
+    date: {
+      en: "FEBRERO 2025 - PRESENT",
+      es: "FEBRERO 2025 - ACTUALMENTE",
+    },
+  },
+  {
     title: { en: "IT Technician at Infoserv", es: "Técnico de IT en Infoserv" },
     description: {
-      es: "Gestión y soporte en más de 100 redes corporativas a nivel nacional, administración de redes, VoIP, servidores, bases de datos y sistemas operativos. Configuración de routers, VPNs, controles de acceso, switches, máquinas virtuales y puntos de acceso para garantizar operaciones de red seguras y eficientes, con un enfoque en brindar a los usuarios un servicio confiable y de alta calidad.",
-      en: "Management and support of over 100 corporate networks nationwide, administration of networks, VoIP, servers, databases, and operating systems. Configuration of routers, VPNs, access controls, switches, virtual machines, and access points to ensure secure and efficient network operations, with a focus on delivering reliable and high-quality service to users.",
+      es: "Gestión y soporte en más de 100 redes corporativas a nivel nacional, administración de redes, VoIP, servidores, bases de datos y sistemas operativos. Configuración de routers, VPNs, controles de acceso, switches, máquinas virtuales y puntos de acceso para garantizar operaciones de red seguras y eficientes.",
+      en: "Management and support of over 100 corporate networks nationwide, administration of networks, VoIP, servers, databases, and operating systems. Configuration of routers, VPNs, access controls, switches, virtual machines, and access points to ensure secure and efficient network operations.",
     },
-    date: { en: "OCTOBER, 2023 - PRESENT", es: "OCTUBRE, 2023 - ACTUALMENTE" },
+    date: {
+      en: "OCTOBER 2023 - FEBRUARY 2025",
+      es: "OCTUBRE 2023 - FEBRERO 2025",
+    },
   },
   {
     title: {
@@ -15,10 +29,10 @@ const experienceData: Experience[] = [
       es: "Desarrollador Full-Stack en Abbate Servicios Fúnebres",
     },
     description: {
-      es: "Desarrollo de una aplicación web para consultas de horarios de servicio en www.abbate.uy, utilizando React.js y TailwindCSS para una interfaz responsiva. Implementación de un servidor Node.js con una base de datos SQL para la gestión de datos y despliegue de la aplicación en un entorno contenedor con Docker para escalabilidad y fiabilidad.",
-      en: "Development of a web application for service schedule queries at www.abbate.uy, using React.js and TailwindCSS for a responsive interface. Implementation of a Node.js server with an SQL database for data management and deployment of the application in a containerized environment using Docker for scalability and reliability.",
+      es: "Desarrollo de una aplicación web para consultas de horarios de servicio en www.abbate.uy, utilizando React.js y TailwindCSS para una interfaz responsiva. Implementación de un servidor Node.js con una base de datos SQL en un entorno basado en contenedores Docker para escalabilidad y fiabilidad.",
+      en: "Development of a web application for service schedule queries at www.abbate.uy, using React.js and TailwindCSS for a responsive interface. Implementation of a Node.js server with an SQL database in a containerized environment using Docker for scalability and reliability.",
     },
-    date: { en: "APRIL, 2024 - AUGUST 2024", es: "ABRIL, 2024 - AGOSTO, 2024" },
+    date: { en: "APRIL 2024 - AUGUST 2024", es: "ABRIL 2024 - AGOSTO 2024" },
   },
   {
     title: {
@@ -26,12 +40,12 @@ const experienceData: Experience[] = [
       es: "Pasante como Desarrollador Front-End en Upcamp",
     },
     description: {
-      es: 'Colaboración en equipo para el desarrollo de una simulación frontend de interfaz bancaria llamada "DigitalBank" utilizando React.js y Node.js. Implementación de Tailwind CSS para un diseño limpio y responsive, priorizando la experiencia del usuario. Aplicación de la metodología SCRUM para asegurar un trabajo en equipo efectivo.',
-      en: 'Collaboration with a team to develop a simulated "DigitalBank" banking interface using React.js and Node.js. Implementation of Tailwind CSS for a clean and responsive design, prioritizing user experience. Application of the SCRUM methodology to ensure effective teamwork and timely progress.',
+      es: 'Colaboración en equipo para el desarrollo de una simulación frontend de interfaz bancaria llamada "DigitalBank" utilizando React.js y Node.js. Implementación de Tailwind CSS para un diseño limpio y responsive. Aplicación de la metodología SCRUM para asegurar un trabajo en equipo efectivo.',
+      en: 'Collaboration with a team to develop a simulated "DigitalBank" banking interface using React.js and Node.js. Implementation of Tailwind CSS for a clean and responsive design. Application of the SCRUM methodology to ensure effective teamwork and timely progress.',
     },
     date: {
-      en: "MARCH, 2022 - MAY 2024",
-      es: "MARZO, 2022 - MAYO, 2024",
+      en: "MARCH 2022 - MAY 2022",
+      es: "MARZO 2022 - MAYO 2022",
     },
   },
 ];
